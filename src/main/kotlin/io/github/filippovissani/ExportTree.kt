2e52590689a168d338e53ea8b69c65b339f59307
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-10-25 16:21:42 +0200

    refactor: change ExportTree factory signature

--- src/main/kotlin/io/github/filippovissani/ExportTree.kt
+++ src/main/kotlin/io/github/filippovissani/ExportTree.kt
@@ -1,14 +1,17 @@
 package io.github.filippovissani
 
 interface ExportTree<T> {
+    val root: T
+
+    val children: Map<Slot, ExportTree<*>>
     fun followPath(path: List<Slot>): ExportTree<*>?
 
     companion object {
-        operator fun <T> invoke(root: T, children: Map<Slot, ExportTree<*>>): ExportTree<T> = ExportTreeImpl(root, children)
+        operator fun <T> invoke(root: T, children: Sequence<Pair<Slot, ExportTree<*>>> = emptySequence()): ExportTree<T> = ExportTreeImpl(root, children.toMap())
     }
 }
 
-internal class ExportTreeImpl<T>(val root: T, val children: Map<Slot, ExportTree<*>>) : ExportTree<T>{
+internal class ExportTreeImpl<T>(override val root: T, override val children: Map<Slot, ExportTree<*>>) : ExportTree<T>{
     override fun followPath(path: List<Slot>): ExportTree<*>? = when {
             path.isNotEmpty() -> children[path.first()]?.followPath(path.drop(1))
             else -> this
