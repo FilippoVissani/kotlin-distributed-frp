0656b57dbcca4a4a301144959dd92360480bd93e
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-10-30 11:24:05 +0100

    test: improve SemanticsSpec tests

--- src/test/kotlin/io/github/filippovissani/kotlin_distributed_frp/SemanticsSpec.kt
+++ src/test/kotlin/io/github/filippovissani/kotlin_distributed_frp/SemanticsSpec.kt
@@ -1,36 +1,88 @@
 package io.github.filippovissani.kotlin_distributed_frp
 
 import io.github.filippovissani.kotlin_distributed_dfrp.*
+import io.github.filippovissani.kotlin_distributed_dfrp.Semantics.branch
+import io.github.filippovissani.kotlin_distributed_dfrp.Semantics.constant
+import io.github.filippovissani.kotlin_distributed_dfrp.Semantics.selfID
 import io.kotest.common.runBlocking
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
 import kotlin.test.*
 
-class SemanticsSpec {
+class SemanticsSpec : FreeSpec({
+    val selfID = DeviceID(1)
+    val context = Context(selfID)
+    val neighbours = setOf(DeviceID(1), DeviceID(2), DeviceID(3), DeviceID(4))
+    val path = emptyList<Nothing>()
+    val thenValue = 1
+    val elseValue = 2
 
-    private val selfID = DeviceID(1)
-    private val context = Context(selfID)
-
-    @Test
-    fun constant(){
+    fun runFlowOnNeighbors(computation: Computation<Any>, neighbors: Iterable<DeviceID> = neighbours) {
         runBlocking {
-            val value = 10
-            Semantics
-                .constant(value)
-                .run(emptyList(), context)
-                .collect{ export ->
-                    assertEquals(export, ExportTree(value))
+            neighbors.forEach{ neighbour ->
+                val neighbourContext = Context(neighbour)
+                computation.run(path, neighbourContext).collect{ export ->
+                    context.receiveExport(neighbour, export)
                 }
+            }
         }
     }
 
-    @Test
-    fun selfID(){
-        runBlocking {
-            Semantics
-                .selfID()
-                .run(emptyList(), context)
-                .collect{export ->
-                    assertEquals(export, ExportTree(selfID))
-                }
+    "The constant construct" - {
+        "should be a constant flow with the given value" {
+            runBlocking {
+                val value = 10
+                val computation = constant(value)
+                computation
+                    .run(path, context)
+                    .collect{ export ->
+                        export shouldBe ExportTree(value)
+                    }
+            }
         }
     }
-}
+
+    "The selfID construct" - {
+        "should be a constant flow with the device ID" {
+            runBlocking {
+                val computation = selfID()
+                computation
+                    .run(path, context)
+                    .collect{export ->
+                        export shouldBe ExportTree(selfID)
+                    }
+            }
+        }
+    }
+
+    "The branch construct" - {
+        "should include only the 'then' branch when the condition is true" {
+            runBlocking {
+                val computation = branch(constant(true), constant(thenValue), constant(elseValue))
+                computation.run(path, context).collect{ export ->
+                    assertEquals(export, ExportTree(thenValue, sequenceOf(Condition to ExportTree(true), Then to ExportTree(thenValue))))
+                }
+            }
+        }
+
+        "should include only the 'else' branch when the condition is false" {
+            runBlocking {
+                val computation = branch(constant(false), constant(thenValue), constant(elseValue))
+                computation.run(path, context).collect{ export ->
+                    assertEquals(export, ExportTree(elseValue, sequenceOf(Condition to ExportTree(false), Else to ExportTree(elseValue))))
+                }
+            }
+        }
+
+        "should react to changes in the condition" {
+            val condition = flowOf(true).map { _ -> false }
+            val computation = branch(Computation.fromFlow { _ -> condition }, constant(thenValue), constant(elseValue))
+            val exports = computation.run(path, context)
+            exports.collectLatest { export ->
+                export.root shouldBe elseValue }
+        }
+    }
+})
