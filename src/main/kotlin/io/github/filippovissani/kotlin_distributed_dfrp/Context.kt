b9462fabb6c45d56078713c0f4be83cec8f5e500
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-10-31 10:59:16 +0100

    refactor: now Context emits exports

--- src/main/kotlin/io/github/filippovissani/kotlin_distributed_dfrp/Context.kt
+++ src/main/kotlin/io/github/filippovissani/kotlin_distributed_dfrp/Context.kt
@@ -1,30 +1,32 @@
 package io.github.filippovissani.kotlin_distributed_dfrp
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emptyFlow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.runBlocking
 
 interface Context {
     val selfID: DeviceID
 
     val neighboursStates: Flow<Map<DeviceID, NeighbourState>>
 
-    fun receiveExport(neighborID: DeviceID, exported: Export<Any>): Context
+    fun receiveExport(neighborID: DeviceID, exported: Export<*>)
 
     companion object{
-        operator fun invoke(selfID: DeviceID = DeviceID(),
-                            neighboursStates: Flow<Map<DeviceID, NeighbourState>> = emptyFlow(),
+        operator fun invoke(selfID: DeviceID = DeviceID()
         ): Context{
-            return ContextImpl(selfID, neighboursStates)
+            return ContextImpl(selfID)
         }
     }
 }
 
-internal class ContextImpl(
-    override val selfID: DeviceID,
-    override val neighboursStates: Flow<Map<DeviceID, NeighbourState>>,
-) : Context {
-    override fun receiveExport(neighborID: DeviceID, exported: Export<Any>): Context {
-        return Context(selfID, neighboursStates.map { it.plus(neighborID to NeighbourState(neighborID, exported)) })
+internal class ContextImpl(override val selfID: DeviceID) : Context {
+    private val _neighboursStates = MutableStateFlow(emptyMap<DeviceID, NeighbourState>())
+    override val neighboursStates = _neighboursStates.asSharedFlow()
+
+    override fun receiveExport(neighborID: DeviceID, exported: Export<*>) {
+        runBlocking {
+            _neighboursStates.emit(mapOf(neighborID to NeighbourState(neighborID, exported)))
+        }
     }
 }
