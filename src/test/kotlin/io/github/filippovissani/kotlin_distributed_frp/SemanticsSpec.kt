4c454dea911047848e134a4cec164c6bb6fbe2b2
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-11-02 11:53:18 +0100

    test: simplify SemanticsSpec

--- src/test/kotlin/io/github/filippovissani/kotlin_distributed_frp/SemanticsSpec.kt
+++ src/test/kotlin/io/github/filippovissani/kotlin_distributed_frp/SemanticsSpec.kt
@@ -13,13 +13,13 @@ import kotlin.test.*
 
 class SemanticsSpec : FreeSpec({
     val selfID = 1
-    val context = Context(selfID)
+    val selfContext = Context(selfID)
     val neighbours = setOf(1, 2, 3, 4)
     val path = emptyList<Nothing>()
     val thenValue = 1
     val elseValue = 2
 
-    fun runProgramOnNeighbours(selfContext: Context, computation: Computation<*>, neighbors: Iterable<DeviceID> = neighbours) {
+    fun runProgramOnNeighbours(computation: Computation<*>, neighbors: Iterable<DeviceID> = neighbours) {
         runBlocking {
             neighbors.forEach{ neighbour ->
                 val neighbourContext = Context(neighbour)
@@ -34,7 +34,7 @@ class SemanticsSpec : FreeSpec({
                 val value = 10
                 val program = constant(value)
                 program
-                    .run(path, context)
+                    .run(path, selfContext)
                     .collect{ export ->
                         export shouldBe ExportTree(value)
                     }
@@ -47,7 +47,7 @@ class SemanticsSpec : FreeSpec({
             runBlocking {
                 val program = selfID()
                 program
-                    .run(path, context)
+                    .run(path, selfContext)
                     .collect{export ->
                         export shouldBe ExportTree(selfID)
                     }
@@ -59,7 +59,7 @@ class SemanticsSpec : FreeSpec({
         "should include only the 'then' branch when the condition is true" {
             runBlocking {
                 val program = branch(constant(true), constant(thenValue), constant(elseValue))
-                program.run(path, context).collect{ export ->
+                program.run(path, selfContext).collect{ export ->
                     assertEquals(export, ExportTree(thenValue, mapOf(Condition to ExportTree(true), Then to ExportTree(thenValue))))
                 }
             }
@@ -68,7 +68,7 @@ class SemanticsSpec : FreeSpec({
         "should include only the 'else' branch when the condition is false" {
             runBlocking {
                 val program = branch(constant(false), constant(thenValue), constant(elseValue))
-                program.run(path, context).collect{ export ->
+                program.run(path, selfContext).collect{ export ->
                     assertEquals(export, ExportTree(elseValue, mapOf(Condition to ExportTree(false), Else to ExportTree(elseValue))))
                 }
             }
@@ -78,7 +78,7 @@ class SemanticsSpec : FreeSpec({
             runBlocking {
                 val condition = MutableStateFlow(true)
                 val program = branch(Computation.fromFlow { _ -> condition }, constant(thenValue), constant(elseValue))
-                val exports = program.run(path, context)
+                val exports = program.run(path, selfContext)
                 condition.emit(false)
                 exports.take(1).collectLatest{ export ->
                     export.root shouldBe elseValue
@@ -90,7 +90,7 @@ class SemanticsSpec : FreeSpec({
             runBlocking {
                 val thenBranch = MutableStateFlow(thenValue)
                 val program = branch(constant(true), Computation.fromFlow { thenBranch }, constant(elseValue))
-                val exports = program.run(path, context)
+                val exports = program.run(path, selfContext)
                 val newValue = 100
                 thenBranch.emit(newValue)
                 exports.take(1).collectLatest{ export ->
@@ -104,9 +104,9 @@ class SemanticsSpec : FreeSpec({
         "should collect values from aligned neighbors" {
             runBlocking {
                 val program = branch(Computation.fromFlow { ctx -> flowOf(ctx.selfID < 3) }, neighbour(selfID()), neighbour(constant(0)))
-                runProgramOnNeighbours(context, program)
+                runProgramOnNeighbours(program)
                 val expectedNeighborField = neighbours.associateWith { if (it < 3) it else null  }
-                program.run(path, context).collect{ export ->
+                program.run(path, selfContext).collect{ export ->
                     export.followPath(listOf(Then)) shouldBe ExportTree(expectedNeighborField, mapOf(Neighbour to ExportTree(selfID)))
                 }
             }
