484e6368d4c52428362bfa9d93d1e2c5cd6a0958
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-11-07 09:54:55 +0100

    feat: add mux operator

--- src/main/kotlin/io/github/filippovissani/kotlin_distributed_dfrp/core/impl/Semantics.kt
+++ src/main/kotlin/io/github/filippovissani/kotlin_distributed_dfrp/core/impl/Semantics.kt
@@ -16,6 +16,15 @@ object Semantics : Language {
         return context.neighbors.map { neighbors -> neighbors.map { alignWith(it.key, it.value) }.toMap() }
     }
 
+    private fun <T> conditional(condition: AggregateExpression<Boolean>, th: AggregateExpression<T>, el: AggregateExpression<T>, combiner: (Export<Boolean>, Export<T>, Export<T>) -> Export<T>): AggregateExpression<T>{
+        return AggregateExpression.of{ context, path ->
+            val conditionExport = condition.compute(path.plus(Condition), context)
+            val thenExport = th.compute(path.plus(Then), context)
+            val elseExport = el.compute(path.plus(Else), context)
+            combine(conditionExport, thenExport, elseExport, combiner)
+        }
+    }
+
     override fun selfID(): AggregateExpression<DeviceID> {
         return AggregateExpression.constant { context -> context.selfID }
     }
@@ -36,15 +45,21 @@ object Semantics : Language {
     }
 
     override fun <T> branch(condition: AggregateExpression<Boolean>, th: AggregateExpression<T>, el: AggregateExpression<T>): AggregateExpression<T> {
-        return AggregateExpression.of { context, path ->
-            val conditionExport = condition.compute(path.plus(Condition), context)
-            val thenExport = th.compute(path.plus(Then), context)
-            val elseExport = el.compute(path.plus(Else), context)
-            combine(conditionExport, thenExport, elseExport) { c, t, e ->
-                val selected = if (c.root) t else e
-                val selectedSlot = if (c.root) Then else Else
-                ExportTree(selected.root, mapOf(Condition to c, selectedSlot to selected))
-            }
+        return conditional(condition, th, el){ c, t, e ->
+            val selected = if(c.root) t else e
+            val selectedSlot = if(c.root) Then else Else
+            ExportTree(selected.root, mapOf(Condition to c, selectedSlot to selected))
+        }
+    }
+
+    override fun <T> mux(
+        condition: AggregateExpression<Boolean>,
+        th: AggregateExpression<T>,
+        el: AggregateExpression<T>
+    ): AggregateExpression<T> {
+        return conditional(condition, th, el){ c, t, e ->
+            val selected = if(c.root) t.root else e.root
+            ExportTree(selected, mapOf(Condition to c, Then to t, Else to e))
         }
     }
 
