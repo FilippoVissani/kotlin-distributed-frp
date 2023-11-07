7fb97dbfcdfd3cf10c4c7f2fb0dc21c5120134f7
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-11-07 09:54:55 +0100

    feat: add mux operator

--- src/main/kotlin/io/github/filippovissani/kotlin_distributed_dfrp/core/Language.kt
+++ src/main/kotlin/io/github/filippovissani/kotlin_distributed_dfrp/core/Language.kt
@@ -7,6 +7,8 @@ interface Language {
     fun <T> neighbor(aggregateExpression: AggregateExpression<T>): AggregateExpression<NeighborField<T>>
     
     fun <T> branch(condition: AggregateExpression<Boolean>, th: AggregateExpression<T>, el: AggregateExpression<T>): AggregateExpression<T>
+
+    fun <T> mux(condition: AggregateExpression<Boolean>, th: AggregateExpression<T>, el: AggregateExpression<T>): AggregateExpression<T>
     
     fun <T : Any> loop(initial: T, f: (AggregateExpression<T>) -> AggregateExpression<T>): AggregateExpression<T>
 
