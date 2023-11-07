93a6182ad0cbf971c5620a79db3d8978c418c5fc
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-11-07 09:54:38 +0100

    feat: add fun <T1, T2, T3, R> combine for AggregateExpression

--- src/main/kotlin/io/github/filippovissani/kotlin_distributed_dfrp/core/extensions/AggregateExpressionManipulation.kt
+++ src/main/kotlin/io/github/filippovissani/kotlin_distributed_dfrp/core/extensions/AggregateExpressionManipulation.kt
@@ -5,7 +5,7 @@ import io.github.filippovissani.kotlin_distributed_dfrp.core.ExportTree
 import kotlinx.coroutines.flow.map
 
 fun <T, R> AggregateExpression<T>.map(transform: (T) -> R): AggregateExpression<R> {
-    return AggregateExpression.of { context, path ->
+    return AggregateExpression.of { context, path -> 
         this.compute(path, context).map { export -> export.map(transform) }
     }
 }
@@ -14,6 +14,14 @@ fun <T1, T2, R> combine(exp1: AggregateExpression<T1>, exp2: AggregateExpression
     return AggregateExpression.of { context, path ->
         kotlinx.coroutines.flow.combine(exp1.compute(path, context), exp2.compute(path, context)) { result1, result2 ->
             ExportTree(transform(result1.root, result2.root))
+        }
+    }
+}
+
+fun <T1, T2, T3, R> combine(exp1: AggregateExpression<T1>, exp2: AggregateExpression<T2>, exp3: AggregateExpression<T3>, transform: (T1, T2, T3) -> R): AggregateExpression<R> {
+    return AggregateExpression.of { context, path ->
+        kotlinx.coroutines.flow.combine(exp1.compute(path, context), exp2.compute(path, context), exp3.compute(path, context)) { result1, result2, result3 ->
+            ExportTree(transform(result1.root, result2.root, result3.root))
         }
     }
 }
