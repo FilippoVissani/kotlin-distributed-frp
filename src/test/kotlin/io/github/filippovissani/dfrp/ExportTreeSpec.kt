e290ab61f632d5588a72be040220ded326598013
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-11-09 12:10:21 +0100

    test: format code

--- src/test/kotlin/io/github/filippovissani/dfrp/ExportTreeSpec.kt
+++ src/test/kotlin/io/github/filippovissani/dfrp/ExportTreeSpec.kt
@@ -8,12 +8,12 @@ import kotlin.test.assertEquals
 
 class ExportTreeSpec {
     @Test
-    fun root(){
+    fun root() {
         assertEquals(ExportTree(1).root, 1)
     }
 
     @Test
-    fun children(){
+    fun children() {
         val children: Map<Slot, ExportTree<*>> = mapOf(
             Pair(Key("a"), ExportTree(10)),
             Pair(Key("b"), ExportTree(20)),
@@ -22,7 +22,7 @@ class ExportTreeSpec {
     }
 
     @Test
-    fun traversable(){
+    fun traversable() {
         val exp = ExportTree(1)
         assertEquals(exp.followPath(emptyList()), exp)
     }
