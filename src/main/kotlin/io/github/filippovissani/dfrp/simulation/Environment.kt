6014b26bd4bc664a1b90e94d7909be0a4e233c93
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-11-14 10:44:15 +0100

    chore: format code

--- src/main/kotlin/io/github/filippovissani/dfrp/simulation/Environment.kt
+++ src/main/kotlin/io/github/filippovissani/dfrp/simulation/Environment.kt
@@ -7,7 +7,7 @@ interface Environment {
 
     companion object {
         fun manhattanGrid(columns: Int, rows: Int): Environment {
-            return grid(columns, rows){ column, row ->
+            return grid(columns, rows) { column, row ->
                 listOf(
                     Pair(column, row),
                     Pair(column + 1, row),
@@ -18,22 +18,27 @@ interface Environment {
             }
         }
 
-        private fun grid(columns: Int, rows: Int, candidateNeighbors: (Int, Int) -> Iterable<Pair<Int, Int>>): Environment = object : Environment {
+        private fun grid(
+            columns: Int,
+            rows: Int,
+            candidateNeighbors: (Int, Int) -> Iterable<Pair<Int, Int>>
+        ): Environment = object : Environment {
             private fun row(device: Int): Int = device / columns
 
             private fun col(device: Int): Int = device % columns
 
             override val devicesNumber: Int = rows * columns
 
-            override fun position(device: Int): Pair<Double, Double> = Pair(col(device).toDouble(), row(device).toDouble())
+            override fun position(device: Int): Pair<Double, Double> =
+                Pair(col(device).toDouble(), row(device).toDouble())
 
             override fun neighbors(device: Int): Iterable<Int> {
                 val deviceCol = col(device)
                 val deviceRow = row(device)
                 val candidates = candidateNeighbors(deviceCol, deviceRow)
                 return candidates
-                    .filter{(c, r) -> c >= 0 && r >= 0 && c < columns && r < rows}
-                    .map{(c, r) -> r * columns + c}
+                    .filter { (c, r) -> c >= 0 && r >= 0 && c < columns && r < rows }
+                    .map { (c, r) -> r * columns + c }
             }
         }
     }
