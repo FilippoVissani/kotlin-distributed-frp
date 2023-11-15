dcaf1ac6637ebb60511e59f5b4c72094dcbb4ede
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-11-15 16:14:51 +0100

    fix: add a working implementation for Simulator

--- src/main/kotlin/io/github/filippovissani/dfrp/simulation/Simulator.kt
+++ src/main/kotlin/io/github/filippovissani/dfrp/simulation/Simulator.kt
@@ -1,19 +1,30 @@
 package io.github.filippovissani.dfrp.simulation
 
 import io.github.filippovissani.dfrp.core.AggregateExpression
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 
 class Simulator(private val simulation: Simulation) {
     private val logger = LoggerFactory.getLogger(javaClass)
 
-    suspend fun <T> start(aggregateExpression: AggregateExpression<T>) {
-        val exports =
-            simulation.contexts.map { context -> (context.selfID to aggregateExpression.compute(emptyList(), context)) }
-        exports.forEach { (id, export) ->
-            logger.debug("($id -> ${export.first().root})")
-            simulation.environment.neighbors(id).forEach { n ->
-                simulation.contexts[n].receiveExport(id, export.first())
+    fun <T> start(aggregateExpression: AggregateExpression<T>) {
+        runBlocking {
+            val exports =
+                simulation.contexts.map { context ->
+                    (context.selfID to aggregateExpression.compute(
+                        emptyList(),
+                        context
+                    ))
+                }
+            exports.forEach { (id, export) ->
+                simulation.environment.neighbors(id).forEach { neighborID ->
+                    export.onEach {
+                        logger.info("($id -> ${it.root})")
+                        simulation.contexts[neighborID].receiveExport(id, it)
+                    }.launchIn(this)
+                }
             }
         }
     }
