534227a647addf9ec4f12478254e33193f28b903
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-11-16 18:04:18 +0100

    feat: add partial implementation

--- src/main/kotlin/io/github/filippovissani/dfrp/core/Context.kt
+++ src/main/kotlin/io/github/filippovissani/dfrp/core/Context.kt
@@ -1,88 +1,76 @@
 package io.github.filippovissani.dfrp.core
 
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 
 class Context(val selfID: DeviceID) {
-    val neighbors: MutableSet<Context> = mutableSetOf()
-    val selfExport: MutableMap<Path, MutableStateFlow<*>> = mutableMapOf()
-    private val currentPath: MutableList<Slot> = mutableListOf()
+    val neighbors: MutableStateFlow<Set<Context>> = MutableStateFlow(emptySet())
+    val selfExports: MutableStateFlow<Map<Path, StateFlow<*>>> = MutableStateFlow(emptyMap())
+    private val currentPath: MutableStateFlow<List<Slot>> = MutableStateFlow(emptyList())
 
-    fun selfID(): MutableStateFlow<DeviceID> {
+    fun selfID(): StateFlow<DeviceID> {
         val result = MutableStateFlow(selfID)
-        selfExport[currentPath] = result
-        return result
+        selfExports.update { it.plus(currentPath.value to result) }
+        return result.asStateFlow()
     }
 
-    fun <T> constant(value: T): MutableStateFlow<T> {
+    fun <T> constant(value: T): StateFlow<T> {
         val result = MutableStateFlow(value)
-        selfExport[currentPath] = result
-        return result
+        selfExports.update { it.plus(currentPath.value to result) }
+        return result.asStateFlow()
     }
 
-    fun <T> neighbor(expression: MutableStateFlow<T>): MutableStateFlow<Map<DeviceID, T>> {
-        val alignmentPath = currentPath + Slot.Neighbor
-        val neighborValues: Map<DeviceID, MutableStateFlow<T>?> =
-            neighbors.associate { it.selfID to it.selfExport[alignmentPath] as MutableStateFlow<T>? }
-                .plus(selfID to expression)
-        val result: MutableStateFlow<Map<DeviceID, T>> = MutableStateFlow(emptyMap())
-        neighborValues.forEach{ (neighborID, flow) ->
-            flow?.onEach { newValue ->
-                result.update { it.plus(neighborID to newValue) }
-            }
+    fun <T> neighbor(expression: StateFlow<T>): StateFlow<Map<DeviceID, T>> {
+        val oldPath = currentPath.value
+        val alignmentPath = currentPath.value + Slot.Neighbor
+        val result: MutableStateFlow<Map<DeviceID, T>> = MutableStateFlow(mapOf(selfID to expression.value))
+        neighbors.value.forEach { neighbor ->
+            neighbor.selfExports.onEach { export ->
+                result.update { it.plus(neighbor.selfID to export[alignmentPath] as T) }
+            }.launchIn(GlobalScope)
         }
-        selfExport[currentPath] = result
-        currentPath += Slot.Neighbor
+        selfExports.update { it.plus(oldPath to result) }
+        currentPath.update { alignmentPath }
+        selfExports.update { it.plus(alignmentPath to expression) }
         return result
     }
 
     fun <T> branch(
-        condition: MutableStateFlow<Boolean>,
-        th: MutableStateFlow<T>,
-        el: MutableStateFlow<T>
-    ): MutableStateFlow<T> {
+        condition: StateFlow<Boolean>,
+        th: StateFlow<T>,
+        el: StateFlow<T>
+    ): StateFlow<T> {
         TODO()
     }
 
     fun <T> mux(
-        condition: MutableStateFlow<Boolean>,
-        th: MutableStateFlow<T>,
-        el: MutableStateFlow<T>
-    ): MutableStateFlow<T> {
-        val initialSelected = if(condition.value) th.value else el.value
-        val result = MutableStateFlow(initialSelected)
-        condition.onEach {
-            val selected = if(it) th else el
-            selected.onEach { newValue ->
-                result.update { newValue }
-            }
-        }
-        selfExport[currentPath] = result
-        currentPath += Slot.Condition
-        selfExport[currentPath] = condition
-        currentPath += Slot.Then
-        selfExport[currentPath] = th
-        currentPath += Slot.Else
-        selfExport[currentPath] = el
-        return result
+        condition: StateFlow<Boolean>,
+        th: StateFlow<T>,
+        el: StateFlow<T>
+    ): StateFlow<T> {
+        TODO()
     }
 
     fun <T> loop(
         initial: T,
-        f: (MutableStateFlow<T>) -> MutableStateFlow<T>
-    ): MutableStateFlow<T> {
+        f: (StateFlow<T>) -> StateFlow<T>
+    ): StateFlow<T> {
         TODO()
     }
 
-    fun <T> sense(sensorID: SensorID): MutableStateFlow<T> {
+    fun <T> sense(sensorID: SensorID): StateFlow<T> {
         TODO()
     }
 }
 
 fun <T> aggregate(
     contexts: Iterable<Context>,
-    aggregateExpression: Context.() -> MutableStateFlow<T>
+    aggregateExpression: Context.() -> StateFlow<T>
 ) {
     contexts.forEach {
         with(it){
