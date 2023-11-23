51203d25d41ee78c8ef02cf875366a1f549d2b91
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-11-23 15:43:14 +0100

    test: improve tests

--- src/test/kotlin/io/github/filippovissani/dfrp/SemanticsSpec.kt
+++ src/test/kotlin/io/github/filippovissani/dfrp/SemanticsSpec.kt
@@ -9,40 +9,44 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class SemanticsSpec : FreeSpec({
 
     val logger = KotlinLogging.logger {}
 
-    suspend fun <T> computeResult(aggregateExpression: suspend Context.() -> StateFlow<T>) = coroutineScope {
-        val contexts = (0..3).map { Context(it) }
-        contexts.forEach { it.neighbors = contexts.toSet() }
-        val aggregateJob = launch(Dispatchers.Default) {
-            aggregate(contexts) {
-                aggregateExpression()
-            }
-        }
-        logger.info { "#################################" }
-        val exportsJobs = contexts.map { context ->
-            launch(Dispatchers.Default) {
-                context.selfExports.collect { export ->
-                    logger.info { "${context.selfID} -> $export" }
+    suspend fun <T> computeResult(aggregateExpression: suspend Context.() -> StateFlow<T>, runAfter: () -> Unit = {}) =
+        coroutineScope {
+            val contexts = (0..3).map { Context(it) }
+            contexts.forEach { it.neighbors = contexts.toSet() }
+            val aggregateJob = launch(Dispatchers.Default) {
+                aggregate(contexts) {
+                    aggregateExpression()
                 }
             }
+            val exportsJobs = contexts.map { context ->
+                launch(Dispatchers.Default) {
+                    context.selfExports.collect { export ->
+                        logger.info { "${context.selfID} -> $export" }
+                    }
+                }
+            }
+            runAfter()
+            delay(200)
+            aggregateJob.cancelAndJoin()
+            exportsJobs.forEach { it.cancelAndJoin() }
+            logger.info { "#################################" }
         }
-        delay(200)
-        aggregateJob.cancelAndJoin()
-        exportsJobs.forEach { it.cancelAndJoin() }
-    }
 
     "The selfID construct" - {
         "Should be a constant flow with the device ID" {
             runBlocking {
-                computeResult {
-                    selfID()
-                }
+                logger.info { "Should be a constant flow with the device ID" }
+                computeResult({ selfID() })
             }
         }
     }
@@ -50,9 +54,8 @@ class SemanticsSpec : FreeSpec({
     "The constant construct" - {
         "Should be a constant flow with the given value" {
             runBlocking {
-                computeResult {
-                    constant(100)
-                }
+                logger.info { "Should be a constant flow with the given value" }
+                computeResult({ constant(100) })
             }
         }
     }
@@ -60,9 +63,16 @@ class SemanticsSpec : FreeSpec({
     "The neighbor construct" - {
         "Should collect values from aligned neighbors" {
             runBlocking {
-                computeResult {
-                    neighbor(selfID())
-                }
+                logger.info { "Should collect values from aligned neighbors" }
+                computeResult({ neighbor(selfID()) })
+            }
+        }
+
+        "Should react to changes in the neighborhood state" {
+            runBlocking {
+                logger.info { "Should react to changes in the neighborhood state" }
+                val reactiveValue = MutableStateFlow(100)
+                computeResult({ neighbor(reactiveValue.asStateFlow()) }, { reactiveValue.update { 200 } })
             }
         }
     }
@@ -70,9 +80,17 @@ class SemanticsSpec : FreeSpec({
     "The mux construct" - {
         "Should include both branches when the condition is true" {
             runBlocking {
-                computeResult {
-                    mux(constant(true), constant(0), constant(1))
-                }
+                logger.info { "Should include both branches when the condition is true" }
+                computeResult({ mux(constant(true), constant(0), constant(1)) })
+            }
+        }
+    }
+
+    "The mux construct" - {
+        "Should include both branches when the condition is true" {
+            runBlocking {
+                logger.info { "Should include both branches when the condition is false" }
+                computeResult({ mux(constant(false), constant(0), constant(1)) })
             }
         }
     }
