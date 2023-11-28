dd4139114e3e871e114d5f5bdb84d451a003eb31
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-11-28 16:12:44 +0100

    feat: switch to kotlin logging

--- src/main/kotlin/io/github/filippovissani/dfrp/simulation/Simulator.kt
+++ src/main/kotlin/io/github/filippovissani/dfrp/simulation/Simulator.kt
@@ -3,13 +3,14 @@ package io.github.filippovissani.dfrp.simulation
 import io.github.filippovissani.dfrp.core.AggregateExpression
 import io.github.filippovissani.dfrp.core.Language
 import io.github.filippovissani.dfrp.core.aggregate
+import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import org.slf4j.LoggerFactory
 
 class Simulator(private val simulation: Simulation) {
-    private val logger = LoggerFactory.getLogger(javaClass)
+    private val logger = KotlinLogging.logger {}
 
     suspend fun <T> start(aggregateExpression: Language.() -> AggregateExpression<T>) = coroutineScope {
         val exports = aggregate(simulation.contexts, aggregateExpression)
@@ -17,7 +18,7 @@ class Simulator(private val simulation: Simulation) {
             simulation.environment.neighbors(export.index).forEach { neighborID ->
                 launch(Dispatchers.Default) {
                     export.value.collect {
-                        logger.info("(${export.index} -> ${it.root})")
+                        logger.debug { "(${export.index} -> ${it.root})" }
                         simulation.contexts[neighborID].receiveExport(export.index, it)
                     }
                 }
