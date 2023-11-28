c62c37b49327a35254b46ac017490ca13ec26ded
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-11-28 16:07:40 +0100

    refactor: introduce new design

--- src/main/kotlin/io/github/filippovissani/dfrp/core/extensions/AggregateExpressionManipulation.kt
+++ src/main/kotlin/io/github/filippovissani/dfrp/core/extensions/AggregateExpressionManipulation.kt
@@ -6,20 +6,14 @@ import io.github.filippovissani.dfrp.core.NeighborField
 import kotlinx.coroutines.flow.map
 
 fun <T, R> AggregateExpression<T>.map(transform: (T) -> R): AggregateExpression<R> {
-    return AggregateExpression { context, path ->
-        this.compute(path, context).map { export -> export.map(transform) }
-    }
-}
-
-fun <T> AggregateExpression<NeighborField<T>>.withoutSelf(): AggregateExpression<NeighborField<T>> {
-    return AggregateExpression { context, path ->
-        this.compute(path, context).map { export -> export.map { it.minus(context.selfID) } }
+    return AggregateExpression { path ->
+        this.compute(path).map { export -> export.map(transform) }
     }
 }
 
 fun AggregateExpression<NeighborField<Double?>>.minOrNull(): AggregateExpression<Double?> {
-    return AggregateExpression { context, path ->
-        this.compute(path, context).map { export -> export.map { it.values.minByOrNull { x -> x != null } } }
+    return AggregateExpression { path ->
+        this.compute(path).map { export -> export.map { it.values.minByOrNull { x -> x != null } } }
     }
 }
 
@@ -28,8 +22,8 @@ fun <T1, T2, R> combine(
     exp2: AggregateExpression<T2>,
     transform: (T1, T2) -> R
 ): AggregateExpression<R> {
-    return AggregateExpression { context, path ->
-        kotlinx.coroutines.flow.combine(exp1.compute(path, context), exp2.compute(path, context)) { result1, result2 ->
+    return AggregateExpression { path ->
+        kotlinx.coroutines.flow.combine(exp1.compute(path), exp2.compute(path)) { result1, result2 ->
             ExportTree(transform(result1.root, result2.root))
         }
     }
@@ -41,11 +35,11 @@ fun <T1, T2, T3, R> combine(
     exp3: AggregateExpression<T3>,
     transform: (T1, T2, T3) -> R
 ): AggregateExpression<R> {
-    return AggregateExpression { context, path ->
+    return AggregateExpression { path ->
         kotlinx.coroutines.flow.combine(
-            exp1.compute(path, context),
-            exp2.compute(path, context),
-            exp3.compute(path, context)
+            exp1.compute(path),
+            exp2.compute(path),
+            exp3.compute(path)
         ) { result1, result2, result3 ->
             ExportTree(transform(result1.root, result2.root, result3.root))
         }
