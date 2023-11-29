4cd6c32cbb0410d8eff4b5fd58ead198c836fe7b
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-11-29 09:52:55 +0100

    test: add tests for CombinedStateFlow

---
+++ src/test/kotlin/io/github/filippovissani/dfrp/core/extensions/CombinedStateFlowSpec.kt
@@ -0,0 +1,24 @@
+package io.github.filippovissani.dfrp.core.extensions
+
+import io.kotest.core.spec.style.FreeSpec
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlin.test.assertEquals
+
+class CombinedStateFlowSpec : FreeSpec({
+    "CombinedStateFlow" - {
+        "Should update its combined value" {
+            val value1 = 1
+            val value2 = 2
+            val newValue = 5
+            val stateFlow1 = MutableStateFlow(value1)
+            val stateFlow2 = MutableStateFlow(value2)
+            val stateFlowResult = combineStates(stateFlow1, stateFlow2){ states ->
+                states.sum()
+            }
+            assertEquals(value1 + value2, stateFlowResult.value)
+            stateFlow1.update { newValue }
+            assertEquals(newValue + value2, stateFlowResult.value)
+        }
+    }
+})
