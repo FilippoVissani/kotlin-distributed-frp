2e37ca27f25234c6a6f572aadc95b89910c45f22
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-11-30 10:03:15 +0100

    feat: introduce new architecture

--- src/main/kotlin/io/github/filippovissani/dfrp/core/AggregateExpression.kt
+++ src/main/kotlin/io/github/filippovissani/dfrp/core/AggregateExpression.kt
@@ -1,31 +1,30 @@
 package io.github.filippovissani.dfrp.core
 
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.transform
+import io.github.filippovissani.dfrp.core.extensions.mapStates
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 interface AggregateExpression<T> {
-    fun compute(path: Path): Flow<Export<T>>
+    fun compute(path: Path): StateFlow<Export<T>>
 
     companion object {
-        operator fun <T> invoke(f: (Path) -> Flow<Export<T>>): AggregateExpression<T> {
+        operator fun <T> invoke(f: (Path) -> StateFlow<Export<T>>): AggregateExpression<T> {
             return AggregateExpressionImpl(f)
         }
 
-        fun <T> fromFlow(flow: () -> Flow<T>): AggregateExpression<T> {
-            return AggregateExpression { _ -> flow().map { ExportTree(it) } }
+        inline fun <reified T> fromStateFlow(crossinline flow: () -> StateFlow<T>): AggregateExpression<T> {
+            return AggregateExpression { _ -> mapStates(flow()) { state -> ExportTree(state) } }
         }
 
-        fun <T> constant(value: () -> T): AggregateExpression<T> {
-            return fromFlow { flowOf(value()) }
+        inline fun <reified T> constant(crossinline value: () -> T): AggregateExpression<T> {
+            return fromStateFlow { MutableStateFlow(value()) }
         }
     }
 }
 
-internal class AggregateExpressionImpl<T>(private val f: (Path) -> Flow<Export<T>>) : AggregateExpression<T> {
+internal class AggregateExpressionImpl<T>(private val f: (Path) -> StateFlow<Export<T>>) : AggregateExpression<T> {
 
-    override fun compute(path: Path): Flow<Export<T>> {
-        return f(path).transform { emit(it) }
+    override fun compute(path: Path): StateFlow<Export<T>> {
+        return f(path)
     }
 }
--- src/main/kotlin/io/github/filippovissani/dfrp/simulation/Simulation.kt
+++ src/main/kotlin/io/github/filippovissani/dfrp/simulation/Simulation.kt
@@ -4,9 +4,10 @@ import io.github.filippovissani.dfrp.core.Context
 
 class Simulation(val environment: Environment, private val source: Int) {
     val contexts = (0 until environment.devicesNumber).map {
-        if (it == source) Context(
-            it,
-            mapOf(Sensors.IS_SOURCE.sensorID to true)
-        ) else Context(it, mapOf(Sensors.IS_SOURCE.sensorID to false))
+        if (it == source) {
+            Context(it, mapOf(Sensors.IS_SOURCE.sensorID to true))
+        } else {
+            Context(it, mapOf(Sensors.IS_SOURCE.sensorID to false))
+        }
     }
 }
